7554c7c6dc9279aeb235f7c18f50f05b1d6b6683
as7bzx92dd <g04032016@mail.ru>
2016-10-30 21:41:58 +0300

    backbone one

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
-var Person = function(config){
-    this.name = config.name;
-    this.age = config.age;
-    this.job = config.job;
-};
+//var Person = function(config){
+//    this.name = config.name;
+//    this.age = config.age;
+//    this.job = config.job;
+//};
+//
+//Person.prototype.walk = function(){
+//  return this.name + ' is walking';
+//};
+//
+//var nick = new Person({name: 'Nick', age:'24',job:'Front-end-developer'});
 
-Person.prototype.walk = function(){
-  return this.name + ' is walking';
-};
+var Person = Backbone.Model.extend({// на жаргоне это класс
+   defaults: {
+       name: 'Dima',
+       age: 23,
+       job: 'web-developer'
+   }
 
-var nick = new Person({name: 'Nick', age:'24',job:'Front-end-developer'});
+});
+
+var person = new Person;
